15d9cebb8df5db1781316ddbde53f319d64ebf07
Vsevolod021 <mr.seva.021@gmail.com>
2024-06-11 23:33:11 +0300

    layout

--- billing-management/src/App.jsx
+++ billing-management/src/App.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { SnackbarProvider } from 'notistack';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
 
-import Landing from 'src/pages/Landing';
-import Home from 'src/pages/Home';
-import Bills from 'src/pages/Bills';
-import ObjectDistributionPage from 'src/pages/ObjectDistributionPage';
-import Forecast from 'src/pages/Forecast';
-import ResetPassword from 'src/pages/ResetPassword';
-import DistributedPaymentInvoices from 'src/pages/DistributedPaymentInvoices';
-import DistributionManagement from 'src/pages/DistributionManagement';
+import Landing from "src/pages/Landing";
+import Home from "src/pages/Home";
+import Bills from "src/pages/Bills";
+import ObjectDistributionPage from "src/pages/ObjectDistributionPage";
+import Forecast from "src/pages/Forecast";
+import ResetPassword from "src/pages/ResetPassword";
+import DistributedPaymentInvoices from "src/pages/DistributedPaymentInvoices";
+import DistributionManagement from "src/pages/DistributionManagement";
 
-import Header from 'src/components/Header';
-import Sidebar from 'src/components/Sidebar';
-import ProtectedRoute from './ProtectedRoute';
-import './styles/globals.scss'; // Убедитесь, что вы импортируете глобальные стили
+import ProtectedRoute from "./ProtectedRoute";
+import Layout from "./components/Layout";
+
+import "@fontsource/inter/800.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/400.css";
+import "./styles/globals.scss";
 
 const App = () => {
   return (
@@ -27,37 +32,31 @@ const App = () => {
 };
 
 const AppContent = () => {
-  const location = useLocation();
-  const hideHeaderAndSidebar = location.pathname === '/' || location.pathname === '/reset-password';
-
   return (
-    <div className="app-container">
-      {!hideHeaderAndSidebar && <Sidebar />}
-      <main className="content">
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/home" element={<Home />} />
-          {/* 1 */}
+    <div className='app-container'>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/' element={<Layout />}>
+          <Route path='/home' element={<Home />} />
           <Route
-            path="/bills"
+            path='/bills'
             element={
               <ProtectedRoute>
                 <Bills />
               </ProtectedRoute>
             }
           />
-          {/* 2 */}
           <Route
-            path="/objectdistributionpage"
+            path='/objectdistributionpage'
             element={
               <ProtectedRoute>
                 <ObjectDistributionPage />
               </ProtectedRoute>
             }
           />
-          {/* 3 */}
           <Route
-            path="/DistributionManagement"
+            path='/DistributionManagement'
             element={
               <ProtectedRoute>
                 <DistributionManagement />
@@ -65,26 +64,23 @@ const AppContent = () => {
             }
           />
           <Route
-            path="/distributedpaymentinvoices"
+            path='/distributedpaymentinvoices'
             element={
               <ProtectedRoute>
                 <DistributedPaymentInvoices />
               </ProtectedRoute>
             }
           />
-          {/* 5 */}
           <Route
-            path="/forecast"
+            path='/forecast'
             element={
               <ProtectedRoute>
                 <Forecast />
               </ProtectedRoute>
             }
           />
-          {/* 6 */}
-          <Route path="/reset-password" element={<ResetPassword />} />
-        </Routes>
-      </main>
+        </Route>
+      </Routes>
     </div>
   );
 };
--- billing-management/src/components/Sidebar.jsx
+++ billing-management/src/components/Sidebar.jsx
@@ -1,38 +1,74 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from '../styles/components/Sidebar.module.scss';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "../styles/components/Sidebar.module.scss";
+
+const navLinks = [
+  {
+    to: "/home",
+    name: "Главная страница",
+    src: "/icons/home.svg",
+    srcActive: "/icons/home-active.svg",
+    alt: "Home",
+  },
+  {
+    to: "/bills",
+    name: "Счета на оплату",
+    src: "/icons/bills.svg",
+    srcActive: "/icons/bills-active.svg",
+    alt: "Bills",
+  },
+  {
+    to: "/objectdistributionpage",
+    name: "Объекты распределения",
+    src: "/icons/distribution.svg",
+    srcActive: "/icons/distribution-active.svg",
+    alt: "ObjectDistributionPage",
+  },
+  {
+    to: "/DistributionManagement",
+    name: "Управление распределение",
+    src: "/icons/control.svg",
+    srcActive: "/icons/control-active.svg",
+    alt: "Control",
+  },
+  {
+    to: "/distributedpaymentinvoices",
+    name: "Распределённые счета на оплату",
+    src: "/icons/distributed_payment_invoices.svg",
+    srcActive: "/icons/distributed_payment_invoices-active.svg",
+    alt: "Distributed Payment Invoices",
+  },
+  {
+    to: "/forecast",
+    name: "Прогнозирование и контроль затрат",
+    src: "/icons/forecast.svg",
+    srcActive: "/icons/forecast-active.svg",
+    alt: "Forecast",
+  },
+];
 
 const Sidebar = () => {
   return (
     <aside className={styles.sidebar}>
-      <Link to="/home">
-        <img src="/icons/home.svg" alt="Home" className={styles.icon} />
-        Главная страница
-      </Link>
-      <Link to="/bills">
-        <img src="/icons/bills.svg" alt="Bills" className={styles.icon} />
-        Счета на оплату
-      </Link>
-      <Link to="/objectdistributionpage">
-        <img src="/icons/distribution.svg" alt="ObjectDistributionPage" className={styles.icon} />
-        Объекты распределения
-      </Link>
-      <Link to="/DistributionManagement">
-        <img src="/icons/control.svg" alt="Control" className={styles.icon} />
-        Управление распределение
-      </Link>
-      <Link to="/distributedpaymentinvoices">
-        <img
-          src="/icons/distributed_payment_invoices.svg"
-          alt="Distributed Payment Invoices"
-          className={styles.icon}
-        />
-        Распределённые счета на оплату
-      </Link>
-      <Link to="/forecast">
-        <img src="/icons/forecast.svg" alt="Forecast" className={styles.icon} />
-        Прогнозирование и контроль затрат
-      </Link>
+      <h1 className={styles.title}>Имя</h1>
+      {navLinks.map((link) => (
+        <NavLink
+          className={({ isActive }) => [isActive ? styles.activeLink : "", styles.link].join(" ")}
+          to={link.to}
+          key={link.name}
+        >
+          {({ isActive }) => (
+            <>
+              <img
+                src={isActive ? link.srcActive : link.src}
+                alt={link.alt}
+                className={styles.icon}
+              />
+              <span>{link.name}</span>
+            </>
+          )}
+        </NavLink>
+      ))}
     </aside>
   );
 };
