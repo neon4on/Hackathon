790c61194709338d3315e664dac4311f4986e462
Vsevolod021 <mr.seva.021@gmail.com>
2024-06-23 18:40:11 +0300

    table-editing

--- billing-management/src/components/DataTable.jsx
+++ billing-management/src/components/DataTable.jsx
@@ -1,11 +1,40 @@
+import PropTypes from "prop-types";
+
 import React from "react";
 import styles from "../styles/components/DataTable.module.scss";
 
 import cn from "classnames";
 
-const DataTable = ({ data, columns, className = "" }) => {
+const DataTable = ({ data, columns, setData = () => {}, className = "", edit = false }) => {
+  const onAddRow = () => {
+    const newRow = {};
+
+    columns.forEach((col) => (newRow[col.key] = "-"));
+
+    setData([...data, newRow]);
+  };
+
+  const onRemoveRow = (index) => {
+    const newData = [...data];
+    newData.splice(index, 1);
+
+    setData(newData);
+  };
+
+  const onDataChange = (index, key, value) => {
+    const newData = [...data];
+
+    newData[index][key] = value;
+
+    setData(newData);
+  };
+
   return (
-    <div className={cn(styles.dataTable, className)}>
+    <div
+      className={cn(styles.dataTable, className, {
+        [styles.editableTable]: edit,
+      })}
+    >
       <div className={styles.dataTableOverflow}>
         <table>
           <thead>
@@ -19,15 +48,49 @@ const DataTable = ({ data, columns, className = "" }) => {
             {data.map((row, index) => (
               <tr key={index}>
                 {columns.map((column) => (
-                  <td key={column.key}>{row[column.key]}</td>
+                  <td key={column.key}>
+                    {edit ? (
+                      <input
+                        value={row[column.key]}
+                        onChange={(event) => onDataChange(index, column.key, event.target.value)}
+                      />
+                    ) : (
+                      <span>{row[column.key]}</span>
+                    )}
+                  </td>
                 ))}
+                {edit && (
+                  <td>
+                    <button onClick={() => onRemoveRow(index)}>
+                      <img src='icons/trash.svg' />
+                    </button>
+                  </td>
+                )}
               </tr>
             ))}
+            {edit && (
+              <tr>
+                <td className={styles.addRow}>
+                  <button onClick={onAddRow}>
+                    <img src='/icons/plus.svg' />
+                    <span>Добавить строку</span>
+                  </button>
+                </td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
     </div>
   );
+};
+
+DataTable.propTypes = {
+  data: PropTypes.array,
+  setData: PropTypes.func,
+  columns: PropTypes.array,
+  className: PropTypes.string,
+  edit: PropTypes.bool,
 };
 
 export default DataTable;
--- billing-management/src/pages/ObjectDistributionPage.jsx
+++ billing-management/src/pages/ObjectDistributionPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "../components/Header";
 import NavTabs from "../components/NavTabs";
@@ -10,217 +10,85 @@ import cn from "classnames";
 
 import styles from "src/styles/pages/ObjectDistributionPage.module.scss";
 
+const tableData = [
+  {
+    id: 205,
+    startYear: 1995,
+    endYear: 1996,
+    measurementEnd: "см.файл",
+    measurementStart: "см.файл",
+    areaUnit: "кв.метр",
+  },
+  {
+    id: 100,
+    startYear: 1999,
+    endYear: 1996,
+    measurementEnd: "см.файл",
+    measurementStart: "см.файл",
+    areaUnit: "кв.метр",
+  },
+  {
+    id: 1006,
+    startYear: 1998,
+    endYear: 1996,
+    measurementEnd: "см.файл",
+    measurementStart: "см.файл",
+    areaUnit: "кв.метр",
+  },
+];
+
+const columns = [
+  { key: "id", label: "ID здания" },
+  { key: "startYear", label: "Начало владения" },
+  { key: "endYear", label: "Конец владения" },
+  { key: "measurementEnd", label: "Конец действия измерения" },
+  { key: "measurementStart", label: "Начало действия измерения" },
+  { key: "areaUnit", label: "Единица измерения площади" },
+];
+
+const tabs = [
+  { key: "buildings", label: "Справочник зданий" },
+  { key: "assets", label: "Справочник основных средств" },
+  { key: "services", label: "Справочник работ/услуг" },
+  { key: "contracts", label: "Справочник договоров" },
+];
+
 function App() {
   const [activeTab, setActiveTab] = useState("buildings");
+  const [editMoode, setEditMode] = useState(false);
+  const [data, setData] = useState([...tableData]);
+
+  const openEdit = () => setEditMode(true);
+  const closeEdit = () => setEditMode(false);
 
   const handleTabClick = (key) => {
     setActiveTab(key);
   };
 
-  const tabs = [
-    { key: "buildings", label: "Справочник зданий" },
-    { key: "assets", label: "Справочник основных средств" },
-    { key: "services", label: "Справочник работ/услуг" },
-    { key: "contracts", label: "Справочник договоров" },
-  ];
+  const onReset = () => {
+    setData(tableData);
+    closeEdit();
+  };
 
-  const columns = [
-    { key: "id", label: "ID здания" },
-    { key: "startYear", label: "Начало владения" },
-    { key: "endYear", label: "Конец владения" },
-    { key: "measurementEnd", label: "Конец действия измерения" },
-    { key: "measurementStart", label: "Начало действия измерения" },
-    { key: "areaUnit", label: "Единица измерения площади" },
-  ];
-
-  const data = [
-    {
-      id: 205,
-      startYear: 1995,
-      endYear: 1996,
-      measurementEnd: "см.файл",
-      measurementStart: "см.файл",
-      areaUnit: "кв.метр",
-    },
-    {
-      id: 100,
-      startYear: 1999,
-      endYear: 1996,
-      measurementEnd: "см.файл",
-      measurementStart: "см.файл",
-      areaUnit: "кв.метр",
-    },
-    {
-      id: 1006,
-      startYear: 1998,
-      endYear: 1996,
-      measurementEnd: "см.файл",
-      measurementStart: "см.файл",
-      areaUnit: "кв.метр",
-    },
-  ];
+  useEffect(() => setEditMode(false), [activeTab]);
 
   const renderContent = () => {
     switch (activeTab) {
       case "buildings":
         return (
           <>
-            <h2>Массив данных 1. Таблица</h2>
-            <DataTable
-              data={[
-                {
-                  id: 205,
-                  startYear: 1995,
-                  endYear: 1996,
-                  measurementEnd: "см.файл",
-                  measurementStart: "см.файл",
-                  areaUnit: "кв.метр",
-                },
-                {
-                  id: 100,
-                  startYear: 1999,
-                  endYear: 1996,
-                  measurementEnd: "см.файл",
-                  measurementStart: "см.файл",
-                  areaUnit: "кв.метр",
-                },
-                {
-                  id: 1006,
-                  startYear: 1998,
-                  endYear: 1996,
-                  measurementEnd: "см.файл",
-                  measurementStart: "см.файл",
-                  areaUnit: "кв.метр",
-                },
-              ]}
-              columns={[
-                { key: "id", label: "ID здания" },
-                { key: "startYear", label: "Начало владения" },
-                { key: "endYear", label: "Конец владения" },
-                { key: "measurementEnd", label: "Конец действия измерения" },
-                { key: "measurementStart", label: "Начало действия измерения" },
-                { key: "areaUnit", label: "Единица измерения площади" },
-              ]}
-            />
-            <AddButton
-              label='Добавить средство'
-              onClick={() => console.log("Add button clicked")}
-            />
-            <DataTable
-              data={[
-                {
-                  id: 205,
-                  startYear: 1995,
-                  endYear: 1996,
-                  measurementEnd: "см.файл",
-                  measurementStart: "см.файл",
-                  areaUnit: "кв.метр",
-                },
-                {
-                  id: 100,
-                  startYear: 1999,
-                  endYear: 1996,
-                  measurementEnd: "см.файл",
-                  measurementStart: "см.файл",
-                  areaUnit: "кв.метр",
-                },
-                {
-                  id: 1006,
-                  startYear: 1998,
-                  endYear: 1996,
-                  measurementEnd: "см.файл",
-                  measurementStart: "см.файл",
-                  areaUnit: "кв.метр",
-                },
-              ]}
-              columns={[
-                { key: "id", label: "ID здания" },
-                { key: "startYear", label: "Начало владения" },
-                { key: "endYear", label: "Конец владения" },
-                { key: "measurementEnd", label: "Конец действия измерения" },
-                { key: "measurementStart", label: "Начало действия измерения" },
-                { key: "areaUnit", label: "Единица измерения площади" },
-              ]}
-            />
-            <h2>Массив данных 2. Таблица</h2>
-            <DataTable
-              data={[
-                {
-                  id: 205,
-                  startYear: 1995,
-                  endYear: 1996,
-                  measurementEnd: "см.файл",
-                  measurementStart: "см.файл",
-                  areaUnit: "кв.метр",
-                },
-                {
-                  id: 100,
-                  startYear: 1999,
-                  endYear: 1996,
-                  measurementEnd: "см.файл",
-                  measurementStart: "см.файл",
-                  areaUnit: "кв.метр",
-                },
-                {
-                  id: 1006,
-                  startYear: 1998,
-                  endYear: 1996,
-                  measurementEnd: "см.файл",
-                  measurementStart: "см.файл",
-                  areaUnit: "кв.метр",
-                },
-              ]}
-              columns={[
-                { key: "id", label: "ID здания" },
-                { key: "startYear", label: "Начало владения" },
-                { key: "endYear", label: "Конец владения" },
-                { key: "measurementEnd", label: "Конец действия измерения" },
-                { key: "measurementStart", label: "Начало действия измерения" },
-                { key: "areaUnit", label: "Единица измерения площади" },
-              ]}
-            />
-            <AddButton
-              label='Добавить средство'
-              onClick={() => console.log("Add button clicked")}
-            />
-            <DataTable
-              data={[
-                {
-                  id: 205,
-                  startYear: 1995,
-                  endYear: 1996,
-                  measurementEnd: "см.файл",
-                  measurementStart: "см.файл",
-                  areaUnit: "кв.метр",
-                },
-                {
-                  id: 100,
-                  startYear: 1999,
-                  endYear: 1996,
-                  measurementEnd: "см.файл",
-                  measurementStart: "см.файл",
-                  areaUnit: "кв.метр",
-                },
-                {
-                  id: 1006,
-                  startYear: 1998,
-                  endYear: 1996,
-                  measurementEnd: "см.файл",
-                  measurementStart: "см.файл",
-                  areaUnit: "кв.метр",
-                },
-              ]}
-              columns={[
-                { key: "id", label: "ID здания" },
-                { key: "startYear", label: "Начало владения" },
-                { key: "endYear", label: "Конец владения" },
-                { key: "measurementEnd", label: "Конец действия измерения" },
-                { key: "measurementStart", label: "Начало действия измерения" },
-                { key: "areaUnit", label: "Единица измерения площади" },
-              ]}
-            />
+            {editMoode === false ? (
+              <AddButton label='Редактировать' onClick={openEdit} />
+            ) : (
+              <div className={styles.closeEditButtons}>
+                <AddButton label='Сохранить' onClick={closeEdit} />
+                <AddButton label='Закрыть' onClick={onReset} />
+              </div>
+            )}
+            <DataTable data={data} setData={setData} columns={columns} edit={editMoode} />
           </>
         );
+
       case "assets":
         return (
           <>
@@ -303,6 +171,7 @@ function App() {
             />
           </>
         );
+
       case "services":
         return (
           <>
@@ -385,6 +254,7 @@ function App() {
             />
           </>
         );
+
       case "contracts":
         return (
           <>
